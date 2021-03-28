58be37cf59cd56905ad0771c2e9292fb28d75d56
Wolf18rus <Wolf18rus@users.noreply.github.com>
2021-03-28 09:38:47 +0300

    firts commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="./imagies/svg/prev-Arrow.svg" alt=""></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="./imagies/svg/next-Arrow.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+
+    });
+
+
+    $('.our-trip__slider').slick({
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="./imagies/svg/prev-Arrow.svg" alt=""></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="./imagies/svg/next-Arrow.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+
+    });
+
+
+
+});
+
+
+
+
+
+
---
+++ js/main_js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.burger__menu').click(function(event) {
+        $('.burger__menu,list__menu,.header__phone,.menu').toggleClass('active');
+    });
+
+    
+
+});
